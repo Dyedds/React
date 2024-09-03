548c6f1f00a032c63473f64424a51aee7f2d6727
SeongEunGyu <116744770+Dyedds@users.noreply.github.com>
2024-09-04 00:41:04 +0900

    Create Hello.js

---
+++ 성은규/1/1.5/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hello({color, name}){
+    return <div style={{color}}>안녕하세요 {name}</div>
+}
+// 비구조화 할당
+
+Hello.defaultProps = {
+    name : '이름없음'
+}
+
+export default Hello;
